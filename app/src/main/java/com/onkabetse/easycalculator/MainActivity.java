e9e24cd122c4243cb12940f306988ccaedc29200
Onkabetse <gosiameonkabetse077@gmail.com>
2023-09-28 20:49:44 +0200

    commit

--- app/src/main/java/com/onkabetse/easycalculator/MainActivity.java
+++ app/src/main/java/com/onkabetse/easycalculator/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
-import org.mozilla.javascript.*;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 TextView resultsTV,solutionTV;
@@ -71,10 +73,26 @@ MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,b
             dataToCalculate = dataToCalculate + buttonText;
         }
         solutionTV.setText(dataToCalculate);
+        String finalResult = getResult(dataToCalculate);
+
+        if(!finalResult.equals("Err")){
+            resultsTV.setText(finalResult);
+        }
     }
 
-    String getResults(String data){
-        return "Calculated";
+    String getResult(String data) {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if (finalResult.endsWith(".0")) {
+                finalResult = finalResult.replace(".0", "");
+            }
+            return finalResult;
+        } catch (Exception e) {
+            return "Err";
+        }
     }
 
 }
